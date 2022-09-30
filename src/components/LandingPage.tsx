5fab19d3fa73878733e198389d6b3e900fc263e8
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2022-10-01 00:35:57 +0600

    converted to vite

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,31 +1,27 @@
-import React, { useState } from 'react';
-import '../App.css';
-import Questions from './Questions';
+import { useState } from "react";
+import "../App.css";
+import Questions from "./Questions";
 
 export default function LandingPage() {
   const [startGame, setStartGame] = useState(false);
 
   function startQuiz() {
-    setStartGame(prev => !prev);
+    setStartGame((prev) => !prev);
   }
 
   return (
-    <div className='landing-page'>
-      {startGame
-        ?
+    <div className="landing-page">
+      {startGame ? (
         <>
           <Questions startQuiz={startQuiz} />
         </>
-        :
+      ) : (
         <>
           <h1>Quizzical</h1>
           <h3>get as many answers correct as possible</h3>
-          <button onClick={startQuiz} >
-            start game
-          </button>
+          <button onClick={startQuiz}>start game</button>
         </>
-      }
+      )}
     </div>
-  )
-
+  );
 }
---
+++ src/components/Question.tsx
@@ -0,0 +1,55 @@
+import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
+
+function Question({ question, handleScore }) {
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [markedAsCorrect, setMarkedAsCorrect] = useState(false);
+
+  useEffect(() => {
+    function handleAnswer() {
+      if (selectedAnswer === question.correct_answer) {
+        if (!markedAsCorrect) {
+          console.log("correct");
+          setMarkedAsCorrect(true);
+        }
+      }
+      if (selectedAnswer !== question.correct_answer) {
+        if (markedAsCorrect) {
+          console.log("wrong");
+          setMarkedAsCorrect(false);
+        }
+      }
+    }
+    handleAnswer();
+  }, [selectedAnswer]);
+
+  /* style={{ backgroundColor: !gameEnded ? "#58e09a" : '' }} */
+
+  return (
+    <div>
+      <p>{question.question}</p>
+      <button
+        onClick={() => setSelectedAnswer(question.correct_answer)}
+        style={{
+          backgroundColor:
+            question.correct_answer === selectedAnswer ? "#ffff80" : "",
+        }}
+      >
+        {question.correct_answer}
+      </button>
+      {question.incorrect_answers.map((inc_answer) => (
+        <button
+          key={nanoid()}
+          onClick={() => setSelectedAnswer(inc_answer)}
+          style={{
+            backgroundColor: inc_answer === selectedAnswer ? "#ffff80" : "",
+          }}
+        >
+          {inc_answer}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Question;
